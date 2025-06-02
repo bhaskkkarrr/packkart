466f21a786b6aeab16bde21dd6e4e3ff7ba7d94f
bhaskkkarrr <bhaskarchauhan748@gmail.com>
2025-06-02 16:19:31 +0530

    Initial commmit with website files

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Thank you for contacting us! We will get back to you soon.");
+  });
+});
